f9683c9164daa0b2250a0458475bdfee5e3cec1e
CrimsonTLU <chris55@tlu.ee>
2023-02-21 10:47:49 +0200

    Initial setup

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Kasutajaliideste disain 2023
  
+1. npm install -D tailwindcss
+2. npx tailwindcss init
+3. installi VSCode extension Tailwind CSS IntelliSense
---
+++ anett-koopia/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <p className='text-xl text-green-500'>Kui Tailwind töötab, on see tekst roheline</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
