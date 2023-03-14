ebcf40b28de9d76d1242fad9f7db6f23999908f6
CrimsonTLU <chris55@tlu.ee>
2023-03-14 12:19:31 +0200

    header scroll

--- anett-koopia/src/App.js
+++ anett-koopia/src/App.js
@@ -1,20 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react'
 
 function App() {
+
+  const updatePosition = () => {
+    setScrollPosition(window.pageYOffset)
+    console.log("Scroll pos: ", scrollPosition)
+
+    if(scrollPosition > 1) {
+      document.getElementById("header").classList.add('invert', 'bg-black')
+    }
+    else if(scrollPosition < 1) {
+      document.getElementById("header").classList.remove('invert', 'bg-black')
+    }
+    
+  }
+
+  window.addEventListener('scroll', updatePosition)
+  
+  const [scrollPosition, setScrollPosition] = useState(0)
+
   return (
     <div id="content">
 
-      <div id="header" className="flex w-full fixed top-0 z-50 h-20">
+      <div id="header" className={'${scrollPosition > 0 ? "invert" : ""} flex w-full fixed top-0 z-50 h-20'}>
         <div id="header-content" className="flex px-7 w-full">
           <a href="/" className="flex p-1">
-            <img className="w-full h-20" src="https://www.anettkontaveit.ee/assets/anett-logo-white.svg"></img>
+            <img className="w-[90px] h-[46px] lg:w-[137px] lg:h-[79px]" src="https://www.anettkontaveit.ee/assets/anett-logo-white.svg"></img>
           </a>
           <div className="flex justify-center items-center font-normal text-lg text-white pl-2.5 gap-2">
             <p className='font-zonaLight font-normal'>WTA</p>
             <p className="font-black font-zonaBold">61</p>
           </div>
-          <div id="navbar" className="flex flex-row w-full justify-end font-zonaBold font-semibold text-sm tracking-wider">
+          <div id="navbar" className="hidden md:flex flex-row w-full justify-end font-zonaBold font-semibold text-xs tracking-wider">
             <ul className="flex content-end items-center text-white gap-4 text-right pl-7">
               <li className="py-2"><a className="text-right hover:underline underline-offset-8" href="/anettist">Anettist</a></li>
 
@@ -35,7 +54,7 @@ function App() {
         </div>
       </div>
 
-      <div id="hero-content" class="bg-[url('https://www.anettkontaveit.ee/images/main-bg.png')]">
+      <div id="hero-content" className="bg-[url('https://www.anettkontaveit.ee/images/main-bg.png')]">
         <div className="h-[700px] flex justify-center">
           <div id="hero-text" className="absolute left-10 right-1/2 top-1/2 z-90  text-white">
             <h1 className="font-stainy text-9xl font-normal -rotate-6 text-center mb-3">Anett</h1>
@@ -48,12 +67,12 @@ function App() {
               </div>
           </div>
           </div>
-          <div id="hero-image" class="bg-[url('https://www.anettkontaveit.ee/images/anett-kontaveit-main.png')] bg-contain bg-no-repeat z-100 absolute top-28 left-1/3 right-8 w-2/3 h-4/5"></div>
-          <div id="hero-diagonal" class="bg-[#fbfbfb] absolute z-80 top-[655px] -left-24 -right-24 h-52 -rotate-3"></div>
+          <div id="hero-image" className="bg-[url('https://www.anettkontaveit.ee/images/anett-kontaveit-main.png')] bg-contain bg-no-repeat z-100 absolute top-28 left-1/3 right-8 w-2/3 h-4/5"></div>
+          <div id="hero-diagonal" className="bg-[#fbfbfb] absolute z-80 top-[655px] -left-24 -right-24 h-52 -rotate-3"></div>
         </div>
         <div className="flex absolute gap-2 text-white z-100 font-zonaLight font-semibold uppercase text-sm rotate-90 top-[300px] -right-12">
-          <a class="social-bt" target="_blank" href="https://twitter.com/AnettKontaveit">Twitter</a>
-          <a class="social-bt" target="_blank" href="https://www.instagram.com/anett_kontaveit/?hl=en">Instagram</a>
+          <a className="social-bt" target="_blank" href="https://twitter.com/AnettKontaveit">Twitter</a>
+          <a className="social-bt" target="_blank" href="https://www.instagram.com/anett_kontaveit/?hl=en">Instagram</a>
 	      </div>
       </div>
 
