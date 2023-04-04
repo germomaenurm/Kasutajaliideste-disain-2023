9ed2b2c9ddd4aa5e62fab3076ec0d5994b82aa56
CrimsonTLU <chris55@tlu.ee>
2023-04-04 13:37:34 +0300

    sponsors and about desktop

--- anett-koopia/src/App.js
+++ anett-koopia/src/App.js
@@ -27,7 +27,13 @@ function App() {
       <div id="header" className={'${scrollPosition > 0 ? "invert" : ""} flex w-full fixed top-0 z-50 h-20'}>
         <div id="header-content" className="flex px-7 w-full">
           <a href="/" className="flex p-1">
-            <img className="w-[90px] h-[46px] lg:w-[137px] lg:h-[79px]" src="https://www.anettkontaveit.ee/assets/anett-logo-white.svg"></img>
+            {scrollPosition < 1 && 
+              <img className="w-[90px] h-[46px] lg:w-[137px] lg:h-[79px]" src="https://www.anettkontaveit.ee/assets/anett-logo-white.svg"></img>
+            }
+            {scrollPosition > 1 && 
+              <img className="w-[90px] h-[46px] lg:w-[137px] lg:h-[79px] invert" src="https://www.anettkontaveit.ee/assets/anett-logo-black.svg"></img>
+            }
+            
           </a>
           <div className="flex justify-center items-center font-normal text-lg text-white pl-2.5 gap-2">
             <p className='font-zonaLight font-normal'>WTA</p>
@@ -74,6 +80,116 @@ function App() {
           <a className="social-bt" target="_blank" href="https://twitter.com/AnettKontaveit">Twitter</a>
           <a className="social-bt" target="_blank" href="https://www.instagram.com/anett_kontaveit/?hl=en">Instagram</a>
 	      </div>
+      </div>
+
+      <div id="frontpage" className="flex flex-col justify-center">
+          <section id="sponsors" className="bg-white z-150 -rotate-3 w-full">
+            <div id="diagonal-content" className="flex justify-center mx-24">
+              <div id="sponsors-content" className="flex flex-col justify-center px-8 py-11 rotate-3 max-w-[1160px]">
+                <h5 className="font-zonaLight text-black font-normal text-sm text-center">Sponsorid</h5>
+                <div className="hidden md:flex justify-center gap-2">
+				
+                  <div class="logo"><a href="/sponsorid"><img src="//media.voog.com/0000/0041/3736/photos/sponsorid-lacoste.png" alt="Lacoste"></img></a></div>
+                
+                  <div class="logo"><a href="/sponsorid"><img src="//media.voog.com/0000/0041/3736/photos/sponsorid-tallink.png" alt="Tallink"></img></a></div>
+                
+                  <div class="logo"><a href="/sponsorid"><img src="//media.voog.com/0000/0041/3736/photos/sponsorid-telegraaf.png" alt="Hotell Telegraaf"></img></a></div>
+                
+                  <div class="logo"><a href="/sponsorid"><img src="//media.voog.com/0000/0041/3736/photos/sponsorid-porsche.png" alt="Porsche"></img></a></div>
+                
+                  <div class="logo"><a href="/sponsorid"><img src="//media.voog.com/0000/0041/3736/photos/sponsors-zenith.png" alt="Zenith"></img></a></div>
+                
+                  <div class="logo"><a href="/sponsorid"><img src="//media.voog.com/0000/0041/3736/photos/sponsorid-babolat.png" alt="Babolat"></img></a></div>
+                
+                  <div class="logo"><a href="/sponsorid"><img src="//media.voog.com/0000/0041/3736/photos/sponsorid-alexela.png" alt="Alexela"></img></a></div>
+                
+              </div>
+              <div className="flex md:hidden">
+                
+                  <div class="logo"><a href="/sponsorid"><img src="//media.voog.com/0000/0041/3736/photos/sponsorid-lacoste.png" alt="Lacoste"></img></a></div>
+                
+                  <div class="logo"><a href="/sponsorid"><img src="//media.voog.com/0000/0041/3736/photos/sponsorid-tallink.png" alt="Tallink"></img></a></div>
+                
+                  <div class="logo"><a href="/sponsorid"><img src="//media.voog.com/0000/0041/3736/photos/sponsorid-telegraaf.png" alt="Hotell Telegraaf"></img></a></div>
+                
+                  <div class="logo"><a href="/sponsorid"><img src="//media.voog.com/0000/0041/3736/photos/sponsorid-porsche.png" alt="Porsche"></img></a></div>
+                
+                  <div class="logo"><a href="/sponsorid"><img src="//media.voog.com/0000/0041/3736/photos/sponsors-zenith.png" alt="Zenith"></img></a></div>
+                
+                  <div class="logo"><a href="/sponsorid"><img src="//media.voog.com/0000/0041/3736/photos/sponsorid-babolat.png" alt="Babolat"></img></a></div>
+                
+                  <div class="logo"><a href="/sponsorid"><img src="//media.voog.com/0000/0041/3736/photos/sponsorid-alexela.png" alt="Alexela"></img></a></div>
+                
+              </div>
+              </div>
+            </div>
+          </section>
+
+          <div id="frontpage-content" className="flex flex-col w-full z-50">
+            <div id="frontpage-content-about" className="px-8 py-14 flex flex-row justify-between">
+              <div id="about" className="flex flex-col w-1/2">
+                <h2 className="text-5xl text-blue-700 mb-9 font-zonaBold">Anettist</h2>
+                <p>Alustanud ema Ülle Milk’i käe all kuueaastasena treeninguid, jõudsin juba kolm aastat hiljem, oma teisel võistlusaastal esimese tiitlivõiduni. Eesti noorte meistrivõistluste finaalis (T-10) alistasin ligi 2,5 tundi kestnud kolmesetilises raskes heitluses oma tolleaegse peamise koduse konkurendi Mari Kalma 4:6;6:4;6:2.<br/><br/></p>
+                <p>Oli aasta 2004 ja Püünsi kooli 9aastase aktiivse koolitüdrukuna olin lisaks tennisetrennidele jaganud oma aega ka koorilaulule ja rahvatantsule. Nüüd jõudis kätte hetk, kus tantsutrennid said tasutud esinemisega 17. Üldtantsupeol – peab ju iga õige eestlane vähemalt ühel laulu- või tantsupeol kaasa lööma.<br/><br/></p>
+              </div>
+              <div id="facts" className="flex w-1/2 pl-8">
+              <div id="frontpage-facts-desktop" className="hidden lg:flex items-stretch flex-wrap">
+								
+                <div className="flex flex-col relative w-1/2 p-6 justify-center items-center after:content-[''] after:bg-[#e8e8e8] after:w-[1px] after:absolute after:top-4 after:bottom-4 after:right-0 ">
+                  <img src="//media.voog.com/0000/0041/3736/photos/faktid-eesti.svg" alt= "Elukoht" className="h-[80px] py-2"/>
+                  <h5 className="text-center">Elukoht</h5>
+                  <h4 className="text-center">Viimsi vald, Harjumaa, Eesti</h4>
+                </div>
+
+                <div className="flex flex-col relative w-1/2 p-6 justify-center items-center">
+                  <img src="//media.voog.com/0000/0041/3736/photos/faktid-mangib.svg" alt= "Mängib" className="h-[80px] py-2"/>
+                  <h5 className="text-center">Mängib</h5>
+                  <h4 className="text-center">paremakäeline (tagantkäsi kahe käega)</h4>
+                </div>
+              
+                <div className="flex flex-col relative w-1/2 p-6 justify-center items-center after:content-[''] after:bg-[#e8e8e8] after:w-[1px] after:absolute after:top-4 after:bottom-4 after:right-0 berfore:content-[''] before:bg-[#e8e8e8] before:h-[1px] before:absolute before:top-0 before:left-4 before:right-4">
+                  <img src="//media.voog.com/0000/0041/3736/photos/faktid-reketid.svg" alt= "Reketid" className="h-[80px] py-2"/>
+                  <h5 className="text-center">Reketid</h5>
+                  <h4 className="text-center">Babolat</h4>
+                </div>
+              
+                <div className="flex flex-col relative w-1/2 p-6 justify-center items-center berfore:content-[''] before:bg-[#e8e8e8] before:h-[1px] before:absolute before:top-0 before:left-4 before:right-4">
+                  <img src="//media.voog.com/0000/0041/3736/photos/faktid-riietus.svg" alt= "Riietus" className="h-[80px] py-2"/>
+                  <h5 className="text-center">Riietus</h5>
+                  <h4 className="text-center">Lacoste</h4>
+                </div>
+              
+            </div>
+            <div id="frontpage-facts-mobile" className="flex lg:hidden">
+              
+                <div class="fact">
+                  <img src="//media.voog.com/0000/0041/3736/photos/faktid-eesti.svg" alt= "Elukoht"/>
+                  <h5 class="text-center">Elukoht</h5>
+                  <h4 class="text-center">Viimsi vald, Harjumaa, Eesti</h4>
+                </div>
+              
+                <div class="fact">
+                  <img src="//media.voog.com/0000/0041/3736/photos/faktid-mangib.svg" alt= "Mängib"/>
+                  <h5 class="text-center">Mängib</h5>
+                  <h4 class="text-center">paremakäeline (tagantkäsi kahe käega)</h4>
+                </div>
+              
+                <div class="fact">
+                  <img src="//media.voog.com/0000/0041/3736/photos/faktid-reketid.svg" alt= "Reketid"/>
+                  <h5 class="text-center">Reketid</h5>
+                  <h4 class="text-center">Babolat</h4>
+                </div>
+              
+                <div class="fact">
+                  <img src="//media.voog.com/0000/0041/3736/photos/faktid-riietus.svg" alt= "Riietus"/>
+                  <h5 class="text-center">Riietus</h5>
+                  <h4 class="text-center">Lacoste</h4>
+                </div>
+              
+            </div>
+              </div>
+            </div>
+          </div>
       </div>
 
     </div>
